7f58b7b496e9d7dc86bd81b3650005c89b8246a0
yly <liuyang.yuan>
2023-11-09 19:01:17 +0800

    初始化

--- src/main/java/org/mybatis/spring/SqlSessionFactoryBean.java
+++ src/main/java/org/mybatis/spring/SqlSessionFactoryBean.java
@@ -79,10 +79,16 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SqlSessionFactoryBean.class);
 
+  /**
+   * 指定 mybatis-config.xml 路径的 Resource 对象
+   */
   private Resource configLocation;
 
   private Configuration configuration;
 
+  /**
+   * 指定 Mapper 路径的 Resource 数组
+   */
   private Resource[] mapperLocations;
 
   private DataSource dataSource;
@@ -405,6 +411,7 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
     state((configuration == null && configLocation == null) || !(configuration != null && configLocation != null),
               "Property 'configuration' and 'configLocation' can not specified with together");
 
+    // 创建 SqlSessionFactory 对象
     this.sqlSessionFactory = buildSqlSessionFactory();
   }
 
@@ -422,6 +429,7 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
 
     final Configuration targetConfiguration;
 
+    // 初始化 configuration 对象，和设置其 `configuration.variables` 属性
     XMLConfigBuilder xmlConfigBuilder = null;
     if (this.configuration != null) {
       targetConfiguration = this.configuration;
@@ -439,10 +447,14 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
       Optional.ofNullable(this.configurationProperties).ifPresent(targetConfiguration::setVariables);
     }
 
+    // 设置 `configuration.objectFactory` 属性
     Optional.ofNullable(this.objectFactory).ifPresent(targetConfiguration::setObjectFactory);
+    // 设置 `configuration.objectWrapperFactory` 属性
     Optional.ofNullable(this.objectWrapperFactory).ifPresent(targetConfiguration::setObjectWrapperFactory);
+    // 设置 `configuration.vfs` 属性
     Optional.ofNullable(this.vfs).ifPresent(targetConfiguration::setVfsImpl);
 
+    // 设置 `configuration.typeAliasesPackage` 属性
     if (hasLength(this.typeAliasesPackage)) {
       String[] typeAliasPackageArray = tokenizeToStringArray(this.typeAliasesPackage,
           ConfigurableApplicationContext.CONFIG_LOCATION_DELIMITERS);
@@ -453,6 +465,7 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
       });
     }
 
+    // 设置 `configuration.typeAliases` 属性
     if (!isEmpty(this.typeAliases)) {
       Stream.of(this.typeAliases).forEach(typeAlias -> {
         targetConfiguration.getTypeAliasRegistry().registerAlias(typeAlias);
@@ -460,6 +473,7 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
       });
     }
 
+    // 初始化 `configuration.interceptorChain` 属性，即拦截器
     if (!isEmpty(this.plugins)) {
       Stream.of(this.plugins).forEach(plugin -> {
         targetConfiguration.addInterceptor(plugin);
@@ -467,6 +481,7 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
       });
     }
 
+    // 扫描 typeHandlersPackage 包，注册 TypeHandler
     if (hasLength(this.typeHandlersPackage)) {
       String[] typeHandlersPackageArray = tokenizeToStringArray(this.typeHandlersPackage,
           ConfigurableApplicationContext.CONFIG_LOCATION_DELIMITERS);
@@ -483,6 +498,7 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
       });
     }
 
+    // 设置 `configuration.databaseId` 属性
     if (this.databaseIdProvider != null) {//fix #64 set databaseId before parse mapper xmls
       try {
         targetConfiguration.setDatabaseId(this.databaseIdProvider.getDatabaseId(this.dataSource));
@@ -491,8 +507,10 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
       }
     }
 
+    // 设置 `configuration.cache` 属性
     Optional.ofNullable(this.cache).ifPresent(targetConfiguration::addCache);
 
+    // 解析 mybatis-config.xml 配置
     if (xmlConfigBuilder != null) {
       try {
         xmlConfigBuilder.parse();
@@ -504,10 +522,12 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
       }
     }
 
+    // 设置 `configuration.environment` 属性
     targetConfiguration.setEnvironment(new Environment(this.environment,
         this.transactionFactory == null ? new SpringManagedTransactionFactory() : this.transactionFactory,
         this.dataSource));
 
+    // 扫描 Mapper XML 文件们，并进行解析
     if (!isEmpty(this.mapperLocations)) {
       for (Resource mapperLocation : this.mapperLocations) {
         if (mapperLocation == null) {
@@ -517,6 +537,7 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
         try {
           XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(mapperLocation.getInputStream(),
               targetConfiguration, mapperLocation.toString(), targetConfiguration.getSqlFragments());
+          // 解析 Mapper XML 配置
           xmlMapperBuilder.parse();
         } catch (Exception e) {
           throw new NestedIOException("Failed to parse mapping resource: '" + mapperLocation + "'", e);
@@ -529,6 +550,7 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
       LOGGER.debug(() -> "Property 'mapperLocations' was not specified or no matching resources found");
     }
 
+    // 创建 SqlSessionFactory 对象
     return this.sqlSessionFactoryBuilder.build(targetConfiguration);
   }
 
@@ -537,6 +559,7 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
    */
   @Override
   public SqlSessionFactory getObject() throws Exception {
+    // 保证 SqlSessionFactory 对象的初始化
     if (this.sqlSessionFactory == null) {
       afterPropertiesSet();
     }
@@ -561,6 +584,7 @@ public class SqlSessionFactoryBean implements FactoryBean<SqlSessionFactory>, In
   }
 
   /**
+   * 监听 ContextRefreshedEvent 事件，如果 MapperStatement 们，没有都初始化都完成，会抛出 IncompleteElementException 异常
    * {@inheritDoc}
    */
   @Override
